e1a31bf5996e0903a57c684d07ddac9c3fd06fd7
kevin2b <25062561+kevin2b@users.noreply.github.com>
2023-10-30 14:54:59 -0400

    Add function in script to allow playing multiple rounds in a session

--- scripts/script.js
+++ scripts/script.js
@@ -16,17 +16,34 @@ function playRound(playerSelection, computerSelection){
 	let computerChoice = computerSelection[0].toUpperCase() + computerSelection.substring(1).toLowerCase();
 	
 	if (playerChoice == computerChoice){
-	    return `Draw! ${playerChoice} draws with ${computerChoice}.`;
+		console.log(`Draw! ${playerChoice} draws with ${computerChoice}.`);
+		return 0.5;
 	}
 	
 	if (playerChoice == "Rock" && computerChoice == "Scissor" ||
 	  playerChoice == "Paper" && computerChoice == "Rock" ||
 	  playerChoice == "Scissor" && computerChoice == "Paper"){
-		return `You Win! ${playerChoice} beats ${computerChoice}.`;
+		console.log(`You Win! ${playerChoice} beats ${computerChoice}.`);
+		return 1;
 	}
 	else{
-		return `You Lost! ${playerChoice} loses to ${computerChoice}.`;
+		console.log(`You Lost! ${playerChoice} loses to ${computerChoice}.`);
+		return 0;
+	}
+}
+
+function game(){
+	let score = 0;
+	const GAMES_PLAYED = 5;
+	for (let i = 0; i < GAMES_PLAYED; i++){
+		score += playRound(prompt("Rock, Paper or Scissor?"), getComputerChoice());
 	}
 	
-	
+	if (score >= 3){
+		console.log(`You Win! Score is ${score}/${GAMES_PLAYED}`);
+	}
+	else{
+		console.log(`You Lose! Score is ${score}/${GAMES_PLAYED}`);
+	}
+	return;
 }
