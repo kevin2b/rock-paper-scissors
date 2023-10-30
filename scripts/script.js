f3114840e3fa222548181d0c67d385e339eb52a4
kevin2b <25062561+kevin2b@users.noreply.github.com>
2023-10-30 14:39:42 -0400

    Add basic rock paper scissors logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+function getComputerChoice () {
+	let choice = Math.floor(Math.random() * 3);
+	if (choice == 0){
+		return "rock";
+	}
+	else if (choice == 1){
+		return "paper";
+	}
+	else{
+		return "scissor";
+	}
+}
+
+function playRound(playerSelection, computerSelection){
+	let playerChoice = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase();
+	let computerChoice = computerSelection[0].toUpperCase() + computerSelection.substring(1).toLowerCase();
+	
+	if (playerChoice == computerChoice){
+	    return `Draw! ${playerChoice} draws with ${computerChoice}.`;
+	}
+	
+	if (playerChoice == "Rock" && computerChoice == "Scissor" ||
+	  playerChoice == "Paper" && computerChoice == "Rock" ||
+	  playerChoice == "Scissor" && computerChoice == "Paper"){
+		return `You Win! ${playerChoice} beats ${computerChoice}.`;
+	}
+	else{
+		return `You Lost! ${playerChoice} loses to ${computerChoice}.`;
+	}
+	
+	
+}
