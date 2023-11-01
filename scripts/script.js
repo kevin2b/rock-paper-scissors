c42e5d0e13d83f7e9f57db724d30bb9dc411a61b
kevin2b <25062561+kevin2b@users.noreply.github.com>
2023-11-01 16:17:56 -0400

    Linked GUI with rock paper scissors logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,73 @@
+const playerMove = document.querySelector("#player-move");
+const playerRock = document.querySelector(".player-option.rock");
+const playerPaper = document.querySelector(".player-option.paper");
+const playerScissor = document.querySelector(".player-option.scissor");
+const comRock = document.querySelector(".opponent-option.rock");
+const comPaper = document.querySelector(".opponent-option.paper");
+const comScissor = document.querySelector(".opponent-option.scissor");
+
+const message = document.querySelector("#message");
+
+let round = 0;
+
+playerMove.addEventListener("click", (e)=>{
+	const target = e.target;
+	let valid = false;
+	let result = "";
+	switch(target.id){
+		case "rock":
+			result = playRound("rock", getComputerChoice());
+			valid = true;
+			break;
+		case "paper":
+			result = playRound("paper", getComputerChoice());
+			valid = true;
+			break;
+		case "scissor":
+			result = playRound("scissor", getComputerChoice());
+			valid = true;
+			break;
+	}
+	
+	if (valid){
+		round++;
+		resetBorder();
+		target.setAttribute("style", "border-color: red");
+		
+		if(result.computerChoice === "Rock"){
+			comRock.setAttribute("style", "border-color: blue");
+		}
+		else if(result.computerChoice === "Paper"){
+			comPaper.setAttribute("style", "border-color: blue");
+		}
+		else{
+			comScissor.setAttribute("style", "border-color: blue");
+		}
+		
+		message.textContent = `Round ${round}: ${result.message}` ; 
+		
+	}
+});
+
+function resetBorder(){
+	playerRock.setAttribute("style", "border-color: black");
+	playerPaper.setAttribute("style", "border-color: black");
+	playerScissor.setAttribute("style", "border-color: black");
+	comRock.setAttribute("style", "border-color: black");
+	comPaper.setAttribute("style", "border-color: black");
+	comScissor.setAttribute("style", "border-color: black");
+	return;
+}
+
+
+
+
 function getComputerChoice () {
 	let choice = Math.floor(Math.random() * 3);
-	if (choice == 0){
+	if (choice === 0){
 		return "rock";
 	}
-	else if (choice == 1){
+	else if (choice === 1){
 		return "paper";
 	}
 	else{
@@ -14,36 +78,25 @@ function getComputerChoice () {
 function playRound(playerSelection, computerSelection){
 	let playerChoice = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase();
 	let computerChoice = computerSelection[0].toUpperCase() + computerSelection.substring(1).toLowerCase();
-	
-	if (playerChoice == computerChoice){
-		console.log(`Draw! ${playerChoice} draws with ${computerChoice}.`);
-		return 0.5;
-	}
-	
-	if (playerChoice == "Rock" && computerChoice == "Scissor" ||
-	  playerChoice == "Paper" && computerChoice == "Rock" ||
-	  playerChoice == "Scissor" && computerChoice == "Paper"){
-		console.log(`You Win! ${playerChoice} beats ${computerChoice}.`);
-		return 1;
-	}
-	else{
-		console.log(`You Lost! ${playerChoice} loses to ${computerChoice}.`);
-		return 0;
-	}
-}
-
-function game(){
 	let score = 0;
-	const GAMES_PLAYED = 5;
-	for (let i = 0; i < GAMES_PLAYED; i++){
-		score += playRound(prompt("Rock, Paper or Scissor?"), getComputerChoice());
+	let message = "";
+	if (playerChoice === computerChoice){
+		message = `Draw! ${playerChoice} draws with ${computerChoice}.`;
 	}
-	
-	if (score >= 3){
-		console.log(`You Win! Score is ${score}/${GAMES_PLAYED}`);
+	else if (playerChoice === "Rock" && computerChoice === "Scissor" ||
+	  playerChoice === "Paper" && computerChoice === "Rock" ||
+	  playerChoice === "Scissor" && computerChoice === "Paper"){
+		message = `You Win! ${playerChoice} beats ${computerChoice}.`;
+		score = 1;
 	}
 	else{
-		console.log(`You Lose! Score is ${score}/${GAMES_PLAYED}`);
+		message = `You Lost! ${playerChoice} loses to ${computerChoice}.`;
+		score = 0;
 	}
-	return;
+	
+	return {
+		message,
+		score,
+		computerChoice
+	};
 }
